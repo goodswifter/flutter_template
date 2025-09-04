f014b806b2524cffe5e9152bbd7c237d4e382167
钟爱东 <zhongaidong@wanlianyida.com>
2025-09-04 20:58:43 +0800

    fix: 初始化

---
+++ lib/const/r.dart
@@ -0,0 +1,9 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class R {
+  const R._();
+
+  /// ![preview](file:///Users/zad/Documents/flutter/flutter_combat_2025/assets/app_logo.webp)
+  static const String ASSETS_APP_LOGO_WEBP = 'assets/app_logo.webp';
+}
